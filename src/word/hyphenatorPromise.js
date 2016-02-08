611a258ccf232a8f5c7288a9edc8a019ca92196c
April Arcus <april.arcus@gmail.com>
2016-02-07 16:22:51 -0800

    notes

--- src/word/hyphenatorPromise.js
+++ src/word/hyphenatorPromise.js
@@ -1,5 +1,20 @@
 /* @flow */
-/* eslint-env commonjs */
+
+/**
+ * This function is a frustrating source of complexity.
+ *
+ * 1.) In .catch(), we could return (v) => v  instead of (v) => [v] at the
+ *     expense of some complication to the type system. The return value will
+ *     ultimately be flattened anyway, so the array allocation is pure waste.
+ *
+ * 2.) Most of the time, h.hyphenate() will be available synchronously. Since
+ *     this function has no side effects, we might be able to extract some
+ *     additional efficiencies by unleashing Zalgo.
+ *
+ * 3.) Alternatively, we could return an Observable instead of a Promise which
+ *     switches from identity to hyphenate once the language pattern resolves
+ *     and the hypher instance becomes available.
+ */
 
 import { MemoizingHypher as Hypher } from './MemoizingHypher'
 import languagePatternPromise from './languagePatternPromise'
